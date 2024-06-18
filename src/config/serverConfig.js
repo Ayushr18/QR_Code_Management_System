6219a8a6e11ed4318fa5595a8d0ac654ecb88031
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2024-06-18 16:39:25 +0530

    Signup logic with password encruption

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,7 +1,9 @@
 const dotenv = require('dotenv');
+const bycrypt = require('bcrypt')
 
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    SALT: bycrypt.genSaltSync(10)
 }
